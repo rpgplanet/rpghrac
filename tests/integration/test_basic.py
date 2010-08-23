bd2f63faa3d59802f0d27f187c9a633c4ed8ebfa
Almad <bugs@almad.net>
2010-08-23 22:20:41 +0200

    Display proper owner name

--- tests/integration/test_basic.py
+++ tests/integration/test_basic.py
@@ -1,8 +1,11 @@
 # -*- coding: utf-8 -*-
+
 from djangosanetesting import HttpTestCase
 from djangosanetesting.utils import mock_settings, get_live_server_path
 
 from urllib2 import urlopen, HTTPError
+
+from lxml.html import document_fromstring
 
 from rpgcommon.user.user import create_user
 
@@ -26,4 +29,17 @@ class TestUserSubdomainsForcable(HttpTestCase):
 
         res = urlopen(get_live_server_path())
         self.assert_equals(200, res.code)
+
+
+class TestBasicLayout(HttpTestCase):
+
+    def setUp(self):
+        super(TestBasicLayout, self).setUp()
+
+        create_user(u"Testeřík", "xxx", "tester@example.com")
+        self.transaction.commit()
+
+    @mock_settings("RPGHRAC_FORCE_USER_SUBDOMAIN_TO", "testerik")
+    def test_owner_username_present(self):
+        self.assert_equals(u"Testeřík", document_fromstring(urlopen(get_live_server_path()).read().decode('utf-8')).xpath("//a[@id='menu-owner-name']")[0].text)
         
