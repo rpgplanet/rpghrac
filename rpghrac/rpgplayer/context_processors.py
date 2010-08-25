06baf8e24c0af52354ab0cece5fa395a25bfd538
Almad <bugs@almad.net>
2010-08-25 23:32:29 +0200

    Be more 404 friendly (refs #27)

--- rpghrac/rpgplayer/context_processors.py
+++ rpghrac/rpgplayer/context_processors.py
@@ -1,6 +1,13 @@
 
 def is_site_owner(request):
-    return {'is_site_owner' : request.site_owner == request.user }
+    if not hasattr(request, "site_owner"):
+        owner = False
+    else:
+        owner = request.site_owner == request.user
+    return {'is_site_owner' :  owner}
 
 def site_owner(request):
+    # context processor is called even for 404
+    if not hasattr(request, "site_owner"):
+        return {'site_owner' : None }
     return {'site_owner' : request.site_owner }
