92861da74dbaafb0de3b674707ea6453a3ffcd8d
Almad <bugs@almad.net>
2010-08-26 22:37:37 +0200

    Allow republishing (refs #1)

--- rpghrac/zapisnik/zapisnik.py
+++ rpghrac/zapisnik/zapisnik.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+from django.core.exceptions import ImproperlyConfigured
 from datetime import datetime
 
 from django.conf import settings
@@ -28,22 +29,27 @@ class Zapisnik(object):
         self.owner = owner
         self.visitor = visitor
         self.site = site or self.owner.get_profile().site
+        
+        self._root_category = None
 
     @property
     def root_category(self):
-        try:
-            return Category.objects.get(
-                site = self.site,
-                tree_parent = None
-            )
-        except Category.DoesNotExist:
-            return Category.objects.create(
-                site = self.site,
-                tree_path = "",
-                tree_parent = None,
-                title = self.owner.username,
-                slug = slugify(self.owner.username)
-            )
+        if not self._root_category:
+            try:
+                self._root_category = Category.objects.get(
+                    site = self.site,
+                    tree_parent = None
+                )
+            except Category.DoesNotExist:
+                self._root_category = Category.objects.create(
+                    site = self.site,
+                    tree_path = "",
+                    tree_parent = None,
+                    title = self.owner.username,
+                    #TODO: unixize
+                    slug = slugify(self.owner.username)
+                )
+        return self._root_category
 
     @property
     def workshop_category(self):
@@ -115,10 +121,43 @@ class Zapisnik(object):
 
         return article
 
+    def _get_parent_category_path(self, tree_path):
+        return '/'.join([i for i in tree_path.split('/')][:-1])
+
+    def _get_parent_category(self, tree_path):
+        path = self._get_parent_category_path(tree_path)
+        if path == self.root_category.tree_path:
+            return self.root_category
+        else:
+            try:
+                return Category.objects.get(
+                    site = self.site,
+                    tree_path = path
+                )
+            except Category.DoesNotExist:
+                category_dict = [i for i in settings.DYNAMIC_RPGPLAYER_CATEGORIES if i['tree_path'] == path]
+                if len(category_dict) < 1:
+                    raise ImproperlyConfigured("DYNAMIC_RPGPLAYER_CATEGORIES contains category whose parent is not there!")
+
+                category_dict = category_dict[0]
+
+                return Category.objects.create(
+                    site = self.site,
+                    tree_path = category_dict['tree_path'],
+                    tree_parent = self._get_parent_category(category_dict['tree_path']),
+                    title = category_dict['title'],
+                    slug = category_dict['slug']
+                )
+
     def publish_article(self, article, categories):
-        ##### FIXME: This is mockup and shall be fixed with test
+
+        # We shall be more smart about this and only delete unpublished and update
+        # those republished. After someone profiles this or has spare time
+        Placement.objects.filter(publishable = article.publishable_ptr, category__site = self.site).delete()
+
         for category in categories:
             category_dict = [i for i in settings.DYNAMIC_RPGPLAYER_CATEGORIES if i['slug'] == category][0]
+
             placement = Placement.objects.create(
                 publishable = article.publishable_ptr,
                 slug = article.slug,
@@ -126,7 +165,7 @@ class Zapisnik(object):
                 category = Category.objects.get_or_create(
                     site = self.site,
                     tree_path = category_dict['tree_path'],
-                    tree_parent = self.root_category,
+                    tree_parent = self._get_parent_category(category_dict['tree_path']),
                     title = category_dict['title'],
                     slug = category_dict['slug']
                 )[0]
--- tests/unit_project/test_zapisnik/test_publishing.py
+++ tests/unit_project/test_zapisnik/test_publishing.py
@@ -36,7 +36,8 @@ class TestArticleManipulation(DatabaseTestCase):
             "slug" : "rpg",
         },
     ])
-    def test_article_published_by_update(self):
+
+    def test_article_appears_in_proper_listing(self):
         self.zapisnik.publish_article(article=self.article, categories=["rpg"])
 
         # if we take published articles now, we shall find ourselves there
@@ -53,5 +54,76 @@ class TestArticleManipulation(DatabaseTestCase):
 
         self.assert_equals(self.article.publishable_ptr, listings[0].target)
 
-        
+    @mock_settings("DYNAMIC_RPGPLAYER_CATEGORIES", [
+        {
+            "tree_path" : "rpg",
+            "parent_tree_path" : "",
+            "title" : "RPG",
+            "slug" : "rpg",
+        },
+        {
+            "tree_path" : "rpg/drd",
+            "parent_tree_path" : "rpg",
+            "title" : "Dračí Doupě",
+            "slug" : "drd",
+        },
+    ])
+    def test_article_can_be_republished(self):
+        self.zapisnik.publish_article(article=self.article, categories=["rpg"])
+        self.zapisnik.publish_article(article=self.article, categories=["drd"])
 
+        # if we take published articles now, we shall find ourselves there
+        rpg_cat = Category.objects.get(
+            site = self.user.get_profile().site,
+            tree_path = "rpg"
+        )
+
+        drd_cat = Category.objects.get(
+            site = self.user.get_profile().site,
+            tree_path = "rpg/drd"
+        )
+
+        listings = Listing.objects.filter(
+            category = drd_cat
+        )
+
+        # we are in drd
+        self.assert_equals(1, len(listings))
+        self.assert_equals(self.article.publishable_ptr, listings[0].target)
+
+        # but not in rpg anymore
+        self.assert_equals(0, len(Listing.objects.filter(category=rpg_cat)))
+
+
+    @mock_settings("DYNAMIC_RPGPLAYER_CATEGORIES", [
+        {
+            "tree_path" : "rpg",
+            "parent_tree_path" : "",
+            "title" : "RPG",
+            "slug" : "rpg",
+        },
+        {
+            "tree_path" : "rpg/drd",
+            "parent_tree_path" : "rpg",
+            "title" : "Dračí Doupě",
+            "slug" : "drd",
+        },
+    ])
+    def test_article_can_be_republished_in_same_category_too(self):
+        self.zapisnik.publish_article(article=self.article, categories=["rpg"])
+        self.zapisnik.publish_article(article=self.article, categories=["drd", "rpg"])
+
+        # if we take published articles now, we shall find ourselves there
+        rpg_cat = Category.objects.get(
+            site = self.user.get_profile().site,
+            tree_path = "rpg"
+        )
+
+        drd_cat = Category.objects.get(
+            site = self.user.get_profile().site,
+            tree_path = "rpg/drd"
+        )
+
+        # we are in drd
+        self.assert_equals(1, len(Listing.objects.filter(category = drd_cat)))
+        self.assert_equals(1, len(Listing.objects.filter(category=rpg_cat)))
