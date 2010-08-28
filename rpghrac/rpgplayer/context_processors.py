51222cacd322260f12c8e5a1b6870b74c4ebeb67
Almad <bugs@almad.net>
2010-08-28 15:31:34 +0200

    Provide admin/newman for rpgplanet (refs #4)

--- rpghrac/rpgplayer/context_processors.py
+++ rpghrac/rpgplayer/context_processors.py
@@ -10,4 +10,22 @@ def site_owner(request):
     # context processor is called even for 404
     if not hasattr(request, "site_owner"):
         return {'site_owner' : None }
-    return {'site_owner' : request.site_owner }
+
+    root_category = None
+
+    try:
+        from django.conf import settings
+        from django.contrib.sites.models import Site
+        from ella.core.models import Category
+        
+        root_category = Category.objects.get(
+            site = Site.objects.get(pk=settings.SITE_ID),
+            tree_parent = None
+        )
+    except:
+        raise
+
+    return {
+        'site_owner' : request.site_owner,
+        'root_category' : root_category
+    }
