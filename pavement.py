d81fd50b103f58e265261f7b2458c51bbd86d8bf
Almad <bugs@almad.net>
2010-11-28 20:00:29 +0100

    Do not freeze & embed minilib

--- pavement.py
+++ pavement.py
@@ -56,16 +56,6 @@ options(
 )
 
 @task
-def freeze_requirements():
-    sh('pip freeze -r requirements.txt > freezed-requirements.txt')
-    
-
-@task
-@needs('freeze_requirements', 'setuptools.command.sdist')
-def sdist():
-    """ Custom sdist """
-
-@task
 def deploy_production():
     """ Deploy to production server """
     sh('fab deploy')
